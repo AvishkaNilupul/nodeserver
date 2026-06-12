ce6ac1126545b893f206e3633bb25478d20afb51
AvishkaNilupul <avishkanilupul55@gmail.com>
2026-06-12 23:55:04 +0530

    updates

--- config/config.js
+++ config/config.js
@@ -13,8 +13,8 @@ module.exports = {
   PORT: process.env.PORT || 3000,
 
   // Admin key used to gate redeem-code generation (/generate)
-  ADMIN_KEY: required("Avishka123"),
+  ADMIN_KEY: required("ADMIN_KEY"),
 
   // MongoDB connection string
-  MONGO_URI: required("mongodb://avishka:Avishka123@ac-ufnccre-shard-00-00.vsigmq3.mongodb.net:27017,ac-ufnccre-shard-00-01.vsigmq3.mongodb.net:27017,ac-ufnccre-shard-00-02.vsigmq3.mongodb.net:27017/codesDB?ssl=true&replicaSet=atlas-ur8a1h-shard-0&authSource=admin&appName=redeemer"),
+  MONGO_URI: required("MONGO_URI"),
 };
